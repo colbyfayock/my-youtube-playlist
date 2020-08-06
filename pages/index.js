5bc2d374ea706d3ad920a8568097e3dd6e30f568
colbyfayock <fayock@gmail.com>
2020-08-05 20:22:11 -0400

    Step 3: adding video items to the page

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,6 @@ export async function getServerSideProps() {
 }
 
 export default function Home({ data }) {
-  console.log('data', data);
   return (
     <div className={styles.container}>
       <Head>
@@ -24,43 +23,25 @@ export default function Home({ data }) {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          My Talks
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <ul className={styles.grid}>
+          {data.items.map(({ id, snippet = {} }) => {
+            const { title, thumbnails = {}, resourceId = {} } = snippet;
+            const { medium } = thumbnails;
+            return (
+              <li key={id} className={styles.card}>
+                <a href={`https://www.youtube.com/watch?v=${resourceId.videoId}`}>
+                  <p>
+                    <img width={medium.width} height={medium.height} src={medium.url} alt="" />
+                  </p>
+                  <h3>{ title }</h3>
+                </a>
+              </li>
+            )
+          })}
+        </ul>
       </main>
 
       <footer className={styles.footer}>
